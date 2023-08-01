e5f1e93668037c50589e3a7f62d4e77b1ef4e947
Александр Пономаренко <ponomarenko.100299@gmail.com>
2023-08-01 15:54:48 +0300

    create myauth views

--- megano/myauth/urls.py
+++ megano/myauth/urls.py
@@ -1,4 +1,23 @@
+from django.urls import path, include
+
+from .views import (
+    SignInView,
+    SignUpView,
+    ProfileView,
+    ProfilePasswordView,
+
+    signOut,
+    profile_avatar,
+)
+
 
 urlpatterns = [
+    path("profile", ProfileView.as_view(), name="profile"),
+    path("profile/avatar", profile_avatar, name="avatar"),
+    path("profile/password", ProfilePasswordView.as_view(), name="password"),
+
+    path("sign-in", SignInView.as_view(), name="sign-in"),
+    path("sign-up", SignUpView.as_view(), name="sign-up"),
+    path("sign-out", signOut, name="sign-out")
 
 ]
